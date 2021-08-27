da8f98980c78ae8220c132ed2c1aad238566610c
Alex Butakov <butakov.aleksey@gmail.com>
2021-08-26 22:09:39 -0400

    Making master match main.

---
+++ app/src/main/java/EIT/project/calculatorapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package EIT.project.calculatorapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
